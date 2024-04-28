440086107f2c6519b69bb335234f7adaf8c83c51
thanhhai45 <thanhhai@itviec.com>
2024-04-28 20:26:26 +0700

    Init project and middlewares

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./server/app");
+
+const PORT = 3055
+
+const server = app.listen(PORT, () => {
+  console.log(`WSV Ecommerce start with port ${PORT}`)
+})
+
+
+process.on('SIGINT', () => {
+  server.close(() => console.log(`Exit Server Express`))
+  // notify.send(ping...)
+})
---
+++ server/app.js
@@ -0,0 +1,26 @@
+const compression = require('compression');
+const express = require('express');
+const { default: helmet } = require('helmet');
+const morgan = require('morgan');
+const app = express();
+
+// init middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+// app.use(compression());
+
+// init db
+
+// init routes
+
+// handling errors
+
+app.get('/', (req, res, next) => {
+  const strCompress = "Hello FANANANA";
+  return res.status(500).json({
+    messages: 'Welcome to NodeJS',
+    metadata: strCompress.repeat(10000),
+  });
+})
+
+module.exports = app;
