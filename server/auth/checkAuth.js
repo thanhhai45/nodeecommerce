62ea373d046cf8f9c1adcd1a5ebec91ff5be2fa1
thanhhai45 <thanhhai@itviec.com>
2024-05-02 00:25:53 +0700

    Create api key and permission for api

---
+++ server/auth/checkAuth.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const { model } = require('mongoose')
+const { findById } = require('../services/apiKey.service')
+const HEADER = {
+  API_KEY: 'x-api-key',
+  AUTHORIZATION: 'authorization'
+}
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString()
+
+    if (!key) {
+      return res.status(403).json({
+        message: 'Forbidden Error'
+      })
+    }
+
+    // check object key
+    const objKey = await findById(key)
+    if (!objKey) {
+      return res.status(403).json({
+        message: 'Forbidden Error'
+      })
+    }
+
+    req.objKey = objKey
+    return next()
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    if (!req.objKey.permissions) {
+      return res.status(403).json({
+        message: 'Permission Dinied'
+      })
+    }
+
+    console.log(`Permission:: `, req.objKey.permissions)
+    const validPermission = req.objKey.permissions.includes(permission)
+
+    if (!validPermission) {
+      return res.status(403).json({
+        message: 'Permission Dinied'
+      })
+    }
+
+    return next()
+  }
+}
+
+module.exports = {
+  apiKey,
+  permission
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const express = require('express');
+const { apiKey, permission } = require('../auth/checkAuth');
 const router = express.Router();
 
-router.use('/v1/api/', require('./access'))
+// check api key
+router.use(apiKey)
+// check permission api key
+router.use(permission('0000'))
 
-// router.get('', (req, res, next) => {
-//   return res.status(200).json({
-//     messages: 'Welcome to NodeJS',
-//   });
-// })
+router.use('/v1/api/', require('./access'))
 
 module.exports = router 
