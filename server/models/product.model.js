831d7e8ed904cc69a0f4224357075eb2669a0529
thanhhai45 <thanhhai@itviec.com>
2024-06-07 23:20:34 +0700

    feat(Update): Add new model and logic create new product with shop info

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -24,7 +24,8 @@ const productSchema = new Schema({
 const clothingSchema = new Schema({
   brand: { type: String, require: true },
   size: String,
-  material: String
+  material: String,
+  product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
 }, {
   collection: 'clothes',
   timestamps: true
@@ -34,14 +35,28 @@ const clothingSchema = new Schema({
 const electronicSchema = new Schema({
   manufacturer: { type: String, require: true },
   model: String,
-  color: String
+  color: String,
+  product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
 }, {
   collection: 'electronics',
   timestamps: true
 })
+
+// define product type = furniture
+const furnitureSchema = new Schema({
+  brand: { type: String, require: true },
+  size: String,
+  material: String,
+  product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
+}, {
+  collection: 'furnitures',
+  timestamps: true
+})
+
 //Export the model
 module.exports = {
   product: model(DOCUMENT_NAME, productSchema),
   clothing: model('Clothing', clothingSchema),
-  electronic: model('Electronics', electronicSchema)
+  electronic: model('Electronics', electronicSchema),
+  furniture: model('Furniture', furnitureSchema)
 };
--- server/services/product.service.js
+++ server/services/product.service.js
@@ -36,18 +36,21 @@ class Product {
     this.product_attributes = product_attributes;
   }
 
-  async createProduct() {
-    return await product.create(this)
+  async createProduct(product_id) {
+    return await product.create({ ...this, _id: product_id })
   }
 }
 
 // Define sub-class for different product types Clothing
 class Clothing extends Product {
   async createProduct() {
-    const newClothing = await clothing.create(this.product_attributes)
+    const newClothing = await clothing.create({
+      ...this.product_attributes,
+      product_shop: this.product_shop
+    })
     if (!newClothing) throw new BadRequestError('Create new Clothing error')
 
-    const newProduct = await super.createProduct()
+    const newProduct = await super.createProduct(newClothing._id)
     if (!newProduct) throw new BadRequestError('Create new Product error')
 
     return newProduct
@@ -57,10 +60,13 @@ class Clothing extends Product {
 // Define sub-class for different product types Electronic
 class Electronics extends Product {
   async createProduct() {
-    const newElectronic = await electronic.create(this.product_attributes)
+    const newElectronic = await electronic.create({
+      ...this.product_attributes,
+      product_shop: this.product_shop
+    })
     if (!newElectronic) throw new BadRequestError('Create new Electronics error')
 
-    const newProduct = await super.createProduct()
+    const newProduct = await super.createProduct(newElectronic._id)
     if (!newProduct) throw new BadRequestError('Create new Product error')
 
     return newProduct
